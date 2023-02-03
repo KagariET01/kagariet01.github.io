3145712677686b767808fd27e1697d4002635677
ET01 <kagariet01@outlook.com>
2023-02-03 19:58:42 +0800

    temp.js

--- temp.js
+++ temp.js
@@ -1,5 +1,5 @@
 {//get head.html
-	fetch('//head.html')
+	fetch('https://kagariet01.github.io/head.html')
 		.then(response => response.text())
 		.then(text => document.getElementById('top').innerHTML = text);
 }
