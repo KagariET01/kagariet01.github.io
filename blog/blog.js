63cac93d20f0bd2ddbd102d71d163137365d40e3
ET01 <kagariet01@outlook.com>
2023-02-04 00:46:09 +0800

    載入blog功能：完成

--- blog/blog.js
+++ blog/blog.js
@@ -1,10 +1,10 @@
-function loadblog(){//get URL tag and load blog
+{//get URL tag and load blog
 	const thisURL=window.location.search;
 	const thisURLtagtxt=thisURL;
 	console.log(thisURLtagtxt);
 	const URLtag=new URLSearchParams(thisURLtagtxt);
 	const URL_p=URLtag.get('p');//獲取文章編號
-	const postURL='https://kagariet01.github.io/blog/docs/'+URL_p+"post.html";//postURL為要讀取的文章的檔案路徑
+	const postURL='https://kagariet01.github.io/blog/docs/'+URL_p+"/post.html";//postURL為要讀取的文章的檔案路徑
 	
 	document.getElementById('docs').innerHTML = postURL;
 
