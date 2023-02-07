dd6c16e009dcc8e08762c8948e0551808c8664d3
ET01 <kagariet01@outlook.com>
2023-02-07 15:56:42 +0800

    blog null

--- blog/blog.js
+++ blog/blog.js
@@ -4,11 +4,17 @@
 	console.log(thisURLtagtxt);//解析網址，並把結果放到thisURLtagtxt
 	const URLtag=new URLSearchParams(thisURLtagtxt);//URLSearchParams：獲取參數(就是網址?後面的東西)
 	const URL_p=URLtag.get('p');//獲取文章編號
+	if(URL_p==null){
+		URL_p="null";
+	}
 	const postURL='https://kagariet01.github.io/blog/docs/'+URL_p+"/post.html";//postURL為要讀取的文章的檔案路徑
 	//const postURL='./docs/'+URL_p+"/post.html";//postURL為要讀取的文章的檔案路徑
 	
 	//document.getElementById('docs').innerHTML = postURL;
 	console.log(postURL);
+	function thisfile(a){
+		return document.getElementById(a);
+	}
 
 
 
