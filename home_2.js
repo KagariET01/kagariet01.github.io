555a1f96035da07dd3a9dca66094963cf7fbb7d1
ET01 <66681962+KagariET01@users.noreply.github.com>
2023-02-10 13:28:06 +0000

    test blog sys

--- home_2.js
+++ home_2.js
@@ -1,12 +1,61 @@
+var board = "";
 {
-	var fs=require('fs');
-	var board="";
-	fs.readFile(
-		'https://kagariet01.github.io/blog_board.html',
-		function(err,dta){
-			if(err)return;
-			board=dta.toString();
+
+	//load temp
+	var fs_read_temp = new require('fs');
+	var fserr = true;
+	fs_read_temp.readFile(
+		'blog_board.html',
+		function (err, dta) {
+			fserr = err;
+			if (err) {
+				console.log("load err");
+				return;
+			}
+			board = dta.toString();
+//			console.log(board);
 		}
 	)
-	console.log(board);
+
+
+	
+
+
+	//load docs
+	if (!fserr) {
+		//document.getElementById("doc_here").innerHTML = "404 error<br>something went wrong, so we can't load blog";
+	} else {
+		var board_html=document.createElement('div');
+		board_html.innerHTML=board;
+
+
+
+		var i = 1;
+		var fs_load_blog = new require('fs');
+		var blog_list_txt="";
+		var loadend=false;
+		while (!loadend) {
+			var takeurl = "./blog/docs/" + i + "/post.html";
+			console.log(takeurl);
+			fs_load_blog.readFile(
+				takeurl,
+				function (err, dta) {
+					loadend = err;
+					if (err) {
+						loadend=true;
+						return
+					}
+					var addfile=board_html;
+					addfile.getElementById('docs').innerHTML=dta;
+					blog_list_txt=addfile.innerHTML+"\n"+blog_list_txt;
+					//document.getElementById('blog_list').innerHTML=blog_list_txt;
+				}
+			)
+			i++;
+			loadend=true;
+		}
+		console.log(blog_list_txt);
+	}
 }
+
+	
