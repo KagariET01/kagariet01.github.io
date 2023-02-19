da90553d535bef8ff6af9f8b33265a2f48447551
ET01 <kagariet01@outlook.com>
2023-02-19 16:05:01 +0800

    res+blog_sys

--- home.js
+++ home.js
@@ -12,50 +12,46 @@
 }*/
 
 function catcher(url) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("GET", url, false);
-  xhttp.send();
-	if(xhttp.status ==404){
-		return null;
+	var xhttp = new XMLHttpRequest();//xhttp為可讀取檔案的功能變數
+	xhttp.open("GET", url, false);//設定xhttp:動作為讀取，讀取路徑為url，非同步讀取
+	try {
+		xhttp.send();//嘗試發送請求
+		if (xhttp.status == 404) {//如果回傳404(error)
+			return null;
+		} else {
+			return xhttp.responseText;//xhttp.responseText為獲取到的檔案(轉成string型態)
+		}
 	}
-  return xhttp.responseText;
+	catch (error) {
+		return null
+	}
 }
-/*function createHtmlFromString(htmlString) {
-  const html = document.createElement('div');
-  html.innerHTML = htmlString.trim();
-  return html;
-}*/
-function stoHTML(str){
-	const re=document.createElement('div');
-	re.innerHTML=str.trim();
+function stoHTML(str) {
+	const re = document.createElement('div');//re=建立新html變數，初始標籤為div
+	re.innerHTML = str.trim();//將str放入re裡面(trim為去除空格，系統會自動將字串轉換成html格式)
 	return re;
 }
-function HTMLtos(html) {
-  var str = html.toString();
-  return str;
-}
 
-var tmptxt=catcher("https://kagariet01.github.io/blog_tmp.html");
-var tmp=stoHTML(tmptxt);
+
+var tmptxt = catcher("https://kagariet01.github.io/blog_tmp.html");
+var tmp = stoHTML(tmptxt);
 console.log(tmp);
 
+var alldoc = "";
 
-var alldoc="";
-
-if(tmp!=null){
-	var i=1;
-	while(true){
-		var nw=catcher("https://kagariet01.github.io/blog/docs/"+i+"/post.html");
-		if(nw!=null){
-			tmp.querySelector("#docs").innerHTML=nw;
+if (tmp != null) {
+	var i = 1;
+	while (true) {
+		var nw = catcher("https://kagariet01.github.io/blog/docs/" + i + "/post.html");
+		if (nw != null) {
+			tmp.querySelector("#docs").innerHTML = nw;//將獲得的文章放入模板
 			console.log(tmp);
-			var ad=tmp.innerHTML;
-			alldoc=ad+alldoc;
-		}
+			var ad = tmp.innerHTML;
+			alldoc = ad + alldoc;//將文章插入文章列表裡
+		}else break;
 		i++
-		break;
 	}
 }
 
 
-document.getElementById("blog_list").innerHTML=alldoc;
+document.getElementById("blog_list").innerHTML = alldoc;
