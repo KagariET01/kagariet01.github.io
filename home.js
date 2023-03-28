0b9f3237eaa85f1ca52aee55a7c2a6c36efe071a
ET01 <kagariet01@outlook.com>
2023-03-28 17:01:45 +0800

    a

--- home.js
+++ home.js
@@ -25,7 +25,7 @@ function stoHTML(str) {
 	return re;
 }
 function stoJSON(str){
-	var re='{'+str+'}';
+	var re=str;
 	return re;
 }
 
@@ -44,7 +44,7 @@ if (tmp != null) {
 		if (nw != null) {
 			var nwHTML=stoHTML(nw);
 			var post_Tag;
-			post_Tag=stoJSON(nwHTML.querySelector("#tag").innerHTML);
+			post_Tag=stoJSON(nwHTML.querySelector("#jsonData").innerHTML);
 			console.log(post_Tag);
 			if(post_Tag["hide"] && URLtag.get("showhide")!=true)continue;
 			try{
